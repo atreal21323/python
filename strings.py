ca02f46b1dd01df46ad511f40c6fb71feafc9f18
atreal21323 <kiaansehgal@gmail.com>
2026-02-18 17:40:30 +0100

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,23 @@
+letter=' news guy: congrats on winning the swimming championship,how does it feel? ' 
+print(letter.upper())
+
+letterother='winner: yea thanks! i just on the swimming championship and it feels verry good!'
+
+print(letterother.upper())
+
+print('this was example nr1')
+
+
+example2='friend 1: hey man can i borrow your pencil?'
+print(example2.lower())
+
+examplefriend2='friend 2 omg!!!!!! ronaldo just won the worldcup!!!!'
+print(examplefriend2.upper())
+
+print('at the stadium')
+
+newsguy=' newsguy: so... Ronaldo how does it feel to win the world cup???'
+print(newsguy.lower())
+
+Ronaldo=' ronaldo: Id never thought i would win my first world cup but thanks for asking now i am gonna celebrate'
+print(Ronaldo.upper())
