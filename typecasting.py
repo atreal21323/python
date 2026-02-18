a8c966cc8ca8ba7ff14294df3f7ee9a7f50e501a
atreal21323 <kiaansehgal@gmail.com>
2026-02-18 17:11:45 +0100

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,9 @@
+weight= 38.5 
+
+print('weight :', weight)
+print('Data Type of weight is', type(weight))
+
+weight = int(weight)
+
+print('weight :', weight)
+print('Data Type of weight is', type(weight))
