336147810c40433ef5f0192b1faa010191433978
atreal21323 <kiaansehgal@gmail.com>
2026-02-21 10:37:10 +0100

    Add files via upload

---
+++ avererage.py
@@ -0,0 +1,16 @@
+number1 = 100
+number2 = 234
+number3 = 567
+number4 = 190
+number5 = 309
+
+
+
+sum = number1+number2+number3+number4+number5
+print('the sum of all the 5 number is:', sum)
+
+
+
+average = sum/5
+
+print('the avrerage of all the number is:', average)
