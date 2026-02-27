087c6a74d08e3aeebe97016ff674bfcc2815b1ba
atreal21323 <kiaansehgal@gmail.com>
2026-02-27 17:41:05 +0100

    Add files via upload

---
+++ home work temprature.py
@@ -0,0 +1,21 @@
+temp = int(input("What's the temperature today? "))
+print("Temperature HAVE to check..:", temp, "degrees")
+
+
+if temp <= 17:
+    print("WOW you're living the dream! It's a cool, NICE and warm day!", temp, "degrees")
+else:
+    if temp <= 30:
+        print("THIS IS VERRYYY REALAXING WEATHER!", temp, "degrees")
+    else:
+        if temp <= 100:
+            print("THIS IS NOT A DRILL! STAY INDORS AND SUNSCREEN AINT GONNA HELP AND IF YOU EVEN THINK GOEING OUTSIDE THEN YOUR GONNA MELT LIKE ICECREAM")
+        else:
+            print("LOOK IM A COMPUTER AND IM NOT DUMB SO BE REALISTIC!!!!")
+
+
+
+
+
+
+print('is u wanna check out my scratch project then here:  https://scratch.mit.edu/projects/1274691414/ ') 
