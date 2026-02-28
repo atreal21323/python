9e0f36cb8984c83c48231e0f2b766eb2657174a3
atreal21323 <kiaansehgal@gmail.com>
2026-02-28 10:54:45 +0100

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print('all the numbers have boolean=n value as TRUE')
+
+else:
+    print('ATLEAST 1 number has boolean value as FALSE')
+
+
+
+    a = 10
+    b = -10
+    c = 0
+
+if a> 0 or b > 0:
+        print('Either of the number is greater then 0')
+
+else:
+     print('no number is GREATRER then 0')
