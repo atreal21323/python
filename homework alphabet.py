ef6cf78663aaf242ce7c98221c5932717c67dcab
atreal21323 <kiaansehgal@gmail.com>
2026-02-28 19:15:18 +0100

    Add files via upload

---
+++ homework alphabet.py
@@ -0,0 +1,8 @@
+letter = input("Enter a letter: ")
+
+if letter >= "A" and letter <= "Z" or letter >= "a" and letter <= "z":
+    print("Yes THIS IS INDEED A LETTER AS U CAN SEE")
+else:
+    print("No THIS IS NOT A LETTER[S] you can try again ")
+
+print('explonation.....  you have variable LETTER then it will tell you to enter a letter then in line 3 we have if the letter is more or equal to A and if LETTER is less or equal to Z, then you have a OR the letter is less then A and less then Z then this will make sure then it would be a alphabet letter and not a  number ')
