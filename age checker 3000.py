6bc70094bfa61ab8afb386d74a2aaa8bf8da44bb
atreal21323 <kiaansehgal@gmail.com>
2026-03-13 20:05:13 +0100

    Add files via upload

---
+++ age checker 3000.py
@@ -0,0 +1,36 @@
+import time 
+
+THEAGECHECKER300 = int(input( "Welcome to thEeEeEeEe AGE CHECKER 3000 this is one of the best models "
+    "and you are the first one to try the AGE CHECKER 3000 now with a super cool waiting effect!!\nEnter your age: "))
+
+print(" amma BE calculating your age...")
+print("---------------------")
+
+time.sleep(3)
+  
+
+if THEAGECHECKER300 == 69 or THEAGECHECKER300 == 67:
+    print("ACCESS DENIED")
+    print("Really? that number??????? Nice try . Focus on the presentation, not the meme.")
+elif THEAGECHECKER300 == 420:
+    print("ACCESS DENIED")
+    print("420? This isn’t the right place for that!")
+
+
+elif THEAGECHECKER300 >= 10 and THEAGECHECKER300 <= 20:
+    print("ACCESS GRANTED")
+    print("You are in the 10-20 age group!")
+    print("You may enter the BEST OF THE BEST PRESENTATION.")
+    
+elif THEAGECHECKER300 < 10:
+    print("ACCESS DENIED")
+    print("You are too WAYYYYYYY TOO LITTLE TO BE DOING THE 'INAPPROPRIATE' presentation.")
+    print("Come back when you are older!")
+
+else:
+    print("ACCESS DENIED")
+    print("You are too OLD for this group.")
+    print("This program is only for ages 10-20.")
+
+print("---------------------")
+print("Program finished thank you for testing the AGE CHECKER 3000 .")
