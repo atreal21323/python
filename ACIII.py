40d71972ce4fbd7f93022d7ed31dac6a12608c3e
atreal21323 <kiaansehgal@gmail.com>
2026-03-14 10:21:47 +0100

    Add files via upload

---
+++ ACIII.py
@@ -0,0 +1,253 @@
+
+print('for the letter small a is: ', ord('a'))
+
+
+print('for the letter capital A is: ', ord('A'))
+
+
+
+print('for the letter small b is: ', ord('b'))
+
+
+print('for the letter capital B is: ', ord('B'))
+
+
+
+print('for the letter small c is: ', ord('c'))
+
+
+print('for the letter capital C is: ', ord('C'))
+
+
+
+
+
+print('for the letter small d is: ', ord('d'))
+
+
+print('for the letter capital D is: ', ord('D'))
+
+
+
+
+
+print('for the letter small e is: ', ord('e'))
+
+
+print('for the letter capital E is: ', ord('E'))
+
+
+
+
+print('for the letter small f is: ', ord('f'))
+
+
+print('for the letter capital F is: ', ord('F'))
+
+
+
+print('for the letter small g is: ', ord('g'))
+
+
+print('for the letter capital G is: ', ord('G'))
+
+
+
+
+
+print('for the letter small h is: ', ord('h'))
+
+
+print('for the letter capital H is: ', ord('H'))
+
+
+
+
+print('for the letter small i is: ', ord('i'))
+
+
+print('for the letter capital I is: ', ord('I'))
+
+
+
+
+print('for the letter small j is: ', ord('j'))
+
+
+print('for the letter capital J is: ', ord('J'))
+
+
+
+
+
+print('for the letter small k is: ', ord('k'))
+
+
+print('for the letter capital K is: ', ord('K'))
+
+
+
+
+
+print('for the letter small l is: ', ord('l'))
+
+
+print('for the letter capital L is: ', ord('L'))
+
+
+
+
+
+print('for the letter small m is: ', ord('m'))
+
+
+print('for the letter capital M is: ', ord('M'))
+
+
+
+
+
+print('for the letter small n is: ', ord('n'))
+
+
+print('for the letter capital N is: ', ord(''))
+
+
+
+
+print('for the letter small o is: ', ord('o'))
+
+
+print('for the letter capital O is: ', ord('O'))
+
+
+
+
+
+
+print('for the letter small p is: ', ord('p'))
+
+
+print('for the letter capital P is: ', ord('P'))
+
+
+
+
+
+print('for the letter small q is: ', ord('q'))
+
+
+print('for the letter capital Q is: ', ord('Q'))
+
+
+
+
+
+
+print('for the letter small r is: ', ord('r'))
+
+
+print('for the letter capital R is: ', ord('R'))
+
+
+
+
+
+
+
+print('for the letter small s is: ', ord('s'))
+
+
+print('for the letter capital S is: ', ord('S'))
+
+
+
+
+
+
+print('for the letter small t is: ', ord('t'))
+
+
+print('for the letter capital T is: ', ord('T'))
+
+
+
+
+
+
+print('for the letter small u is: ', ord('u'))
+
+
+print('for the letter capital U is: ', ord('U'))
+
+
+
+
+
+
+print('for the letter small v is: ', ord('v'))
+
+
+print('for the letter capital V is: ', ord('V'))
+
+
+
+
+
+
+print('for the letter small w is: ', ord('w'))
+
+
+print('for the letter capital W is: ', ord('W'))
+
+
+
+
+
+print('for the letter small x is: ', ord('x'))
+
+
+print('for the letter capital X is: ', ord('X'))
+
+
+
+
+
+
+print('for the letter small y is: ', ord('y'))
+
+
+print('for the letter capital Y is: ', ord('Y'))
+
+
+
+
+
+
+print('for the letter small z is: ', ord('z'))
+
+
+print('for the letter capital Z is: ', ord('Z'))
+
+
+
+
+
+print('the ORD is used for the ASCII code and the ASCII code is used for the computer to understand the language of the human and the computer')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
