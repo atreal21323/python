eec7f2953ce14e82deaa016555e5ec042cf203ea
atreal21323 <kiaansehgal@gmail.com>
2026-04-08 18:08:36 +0200

    Add files via upload

---
+++ breakkkk.py
@@ -0,0 +1,12 @@
+a = input ("enter a word  >")
+
+for i in a:
+    if (i == 'k' or i== 'K'):
+        print('THE LETTER k HAS BEEN FOUNDDDDDDDD!!!!!!!!!!!!!༼ つ ◕_◕ ༽つ (¬‿¬) (¬‿¬) good job!!!')
+
+        break
+    else:
+        print("NOOOOOOOOO YOU DID NOT FIND THE LETTER K o(一︿一+) oಠ╭╮ಠ ┗|｀O′|┛ (╬▔皿▔)╯")
+
+
+    
