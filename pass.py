705ebba18b1845427e0aa1719dc2842f81edbf66
atreal21323 <kiaansehgal@gmail.com>
2026-04-08 17:55:55 +0200

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,19 @@
+for x in range(21):
+    if x % 20 == 0:
+        print('twist')
+
+
+
+    elif x % 15 == 0:
+        pass
+
+    elif x % 5 == 0:
+        print("FIZZZZZZZZZZZZZZ" )
+
+
+    elif x % 3 == 0:
+        print(" HELLO IM A BEE BUZZZZZZZZZZZZZZZZZ BUZZZZZZZZZZZZ BUZZZZZZZZZZZZ" )
+
+
+    else:
+        print(x)
