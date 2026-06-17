453eb8fc87c929ad4aea33745f05b6046be3fe57
atreal21323 <kiaansehgal@gmail.com>
2026-06-17 18:13:17 +0200

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,20 @@
+class book:
+    def __init__(self,author,book,):
+        self.author = author
+
+    def author(self):
+        print("author> robot32")
+
+    def book(self):
+        print("how robots were invented")
+
+
+
+book = book.book()
+book.book()
+
+author = book.author()
+author.author()
+
+
+    
